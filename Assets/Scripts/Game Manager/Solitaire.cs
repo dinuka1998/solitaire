34c88eae24ef1f5d330dc96cc4f2610be728541d
Dinuka Amarasinghe <42789915+dinuka1998@users.noreply.github.com>
2022-06-05 23:48:59 +0530

    deck view functions implemented
    each click will show 3 cards form the deck

--- Assets/Scripts/Game Manager/Solitaire.cs
+++ Assets/Scripts/Game Manager/Solitaire.cs
@@ -10,6 +10,9 @@ public class Solitaire : MonoBehaviour
     private Sprite[] cardfaces;
 
     [SerializeField]
+    private GameObject deckButton;
+
+    [SerializeField]
     private GameObject cardPrefab;
 
     [SerializeField]
@@ -25,6 +28,9 @@ public class Solitaire : MonoBehaviour
     private List<string>[] bottomRow;
     private List<string>[] topRow;
 
+    private List<string> tripsOnDisplay = new List<string>();
+    private List<List<string>> deckTrips = new List<List<string>>();
+
 
     private List<string> bottomCardRow1 = new List<string>();
     private List<string> bottomCardRow2 = new List<string>();
@@ -36,6 +42,13 @@ public class Solitaire : MonoBehaviour
   
 
     public List<string> deck;
+    public List<string> discardPile = new List<string>();
+
+    private int deckLocation;
+    private int trips;
+    private int tripsRemainder;
+
+    private string CARD_TAG = "Card";
 
     
     // Start is called before the first frame update
@@ -56,15 +69,9 @@ public class Solitaire : MonoBehaviour
 
         deck = GenerateDeck();
         Shuffle(deck);
-
-        //test the deck
-        foreach(string card in deck) {
-
-            Debug.Log(card);
-
-        }
         SolitaireSort();
         StartCoroutine(SolitaireDeal());
+        SortDeckIntoTrips();
 
     }
 
@@ -131,10 +138,22 @@ public class Solitaire : MonoBehaviour
 
                 yOffset += 0.3f;
                 zOffset += 0.03f;
+                discardPile.Add(card);
  
             }
             
         }
+
+        foreach (string card in discardPile) {
+
+            if(deck.Contains(card)) {
+
+                deck.Remove(card);
+
+            }
+            
+        }
+        discardPile.Clear();
 
 
     }
@@ -158,6 +177,104 @@ public class Solitaire : MonoBehaviour
             
         }
 
+    }
+
+    public void SortDeckIntoTrips() {
+
+        trips = deck.Count / 3;
+        tripsRemainder = deck.Count % 3;
+        deckTrips.Clear();
+
+        int modifier = 0;
+        for (int i = 0; i < trips; i++) {
+
+            List<string> myTrips = new List<string>();
+            for (int j = 0; j < 3; j++) {
+
+                myTrips.Add(deck[j + modifier]);
+                
+            }
+
+            deckTrips.Add(myTrips);
+            modifier = modifier + 3;
+
+        }
+
+        if (tripsRemainder != 0) {
+
+            List<string> myRemanders = new List<string>();
+            modifier = 0;
+            for (int k = 0; k < tripsRemainder; k++) {
+
+                myRemanders.Add(deck[deck.Count - tripsRemainder + modifier]);
+                modifier++;
+                
+            }
+
+            deckTrips.Add(myRemanders);
+            trips++;
+
+        }
+
+        deckLocation = 0;
+
+    }
+
+    public void DealFromDeck() {
+
+        foreach (Transform child in deckButton.transform) {
+
+            if(child.CompareTag(CARD_TAG)) {
+
+                deck.Remove(child.name);
+                discardPile.Add(child.name);
+                Destroy(child.gameObject);
+
+            }
+
+        }
+
+        if (deckLocation < trips) {
+
+            //draw 3 cards
+
+            tripsOnDisplay.Clear();
+            float xOffset = 2.5f;
+            float zOffset = -0.2f;
+
+            foreach (string card in deckTrips[deckLocation]) {
+
+                GameObject newTopCard = Instantiate(cardPrefab, new Vector3(deckButton.transform.position.x + xOffset, deckButton.transform.position.y, deckButton.transform.position.z + zOffset), Quaternion.identity, deckButton.transform);
+           
+                xOffset = xOffset + 0.5f;
+                zOffset = zOffset - 0.2f;
+
+                newTopCard.name = card;
+                tripsOnDisplay.Add(card);
+                newTopCard.GetComponent<Selectable>().faceUp = true;
+                
+            }
+            deckLocation++;
+
+        }
+        else {
+            Debug.Log("Now restoking");
+            ReStackTopDeck();
+
+        }
+
+    }
+
+    void ReStackTopDeck() {
+
+        foreach (string card in discardPile) {
+
+            deck.Add(card);
+
+        }
+        discardPile.Clear();
+        SortDeckIntoTrips();
+         
     }
 
 }//class
--- Assets/Scripts/Game Manager/UserInputs.cs
+++ Assets/Scripts/Game Manager/UserInputs.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class UserInputs : MonoBehaviour
 {
+
+    private Solitaire solitaire; 
+
+    private string DECK_TAG = "Deck";
+    private string CARD_TAG = "Card";
+    private string TOP_ROW_TAG = "TopRow";
+    private string BOTTOM_ROW_TAG = "BottomRow";
+
     // Start is called before the first frame update
     void Start()
     {
         
+        solitaire = FindObjectOfType<Solitaire>();
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        
+        GetMouseClick();
+
+    }
+
+
+    void GetMouseClick() {
+
+        if (Input.GetMouseButtonDown(0)) {
+
+            Vector3 mousePositon = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10));
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit) {
+
+                if (hit.collider.CompareTag(DECK_TAG)) {
+
+                    Deck();
+
+                }
+                else if (hit.collider.CompareTag(CARD_TAG)) {
+
+                    Card();
+
+                }
+                else if (hit.collider.CompareTag(TOP_ROW_TAG)) {
+
+                    TopRow();
+
+                }
+                else if (hit.collider.CompareTag(BOTTOM_ROW_TAG)) {
+
+                    BottomRow();
+
+                }
+
+            }
+
+        }
+
+    }
+
+    void Deck() {
+
+        solitaire.DealFromDeck();
+
+    }
+
+    void Card() {
+
+        print("clicked on the Card");
         
     }
+
+   void TopRow() {
+
+        print("clicked on the Top Row");
+        
+    }
+
+   void BottomRow() {
+
+        print("clicked on the Botom Row");
+        
+    }
+
 }
