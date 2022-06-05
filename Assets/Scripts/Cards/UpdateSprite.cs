b37671bd65d5ae7a6e589d1615794470f1b27f8e
Dinuka Amarasinghe <42789915+dinuka1998@users.noreply.github.com>
2022-06-05 21:57:29 +0530

    cards are placed on bottom row
    cards are placing on bottom row accordingly.

--- Assets/Scripts/Cards/UpdateSprite.cs
+++ Assets/Scripts/Cards/UpdateSprite.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class UpdateSprite : MonoBehaviour
 {
+
+    [SerializeField]
+    private Sprite cardFace;
+    [SerializeField]
+    private Sprite cardBack;
+
+    private SpriteRenderer spriteRenderer;
+    private Selectable selectable;
+    private Solitaire solitaire;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+
+        List<string> deck = Solitaire.GenerateDeck();
+        solitaire = FindObjectOfType<Solitaire>();
+
+        int deckCount = 0;
+        foreach (string card in deck) {
+
+            if (this.name == card) {
+
+                cardFace = solitaire.GetCardFaces()[deckCount];
+                break;
+                
+            }
+            deckCount ++;
+            
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        selectable = GetComponent<Selectable>();
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+
+        if (selectable.faceUp == true) {
+
+            spriteRenderer.sprite = cardFace;
+
+        }
+        else {
+
+            spriteRenderer.sprite = cardBack;
+
+        }
         
     }
 }
--- Assets/Scripts/Game Manager/Solitaire.cs
+++ Assets/Scripts/Game Manager/Solitaire.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Solitaire : MonoBehaviour
 {
 
-     [SerializeField]
+    [SerializeField]
     private Sprite[] cardfaces;
+
+    [SerializeField]
+    private GameObject cardPrefab;
+
+    [SerializeField]
+    private GameObject[] bottomCardPossitions;
+    [SerializeField]
+    private GameObject[] topCardPossitions;
 
     [SerializeField]
     private static string[] suits = new string[] {"C", "D", "H", "S"};
     [SerializeField]
     private static string[] values = new string[] {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+    private List<string>[] bottomRow;
+    private List<string>[] topRow;
+
+
+    private List<string> bottomCardRow1 = new List<string>();
+    private List<string> bottomCardRow2 = new List<string>();
+    private List<string> bottomCardRow3 = new List<string>();
+    private List<string> bottomCardRow4 = new List<string>();
+    private List<string> bottomCardRow5 = new List<string>();
+    private List<string> bottomCardRow6 = new List<string>();
+    private List<string> bottomCardRow7 = new List<string>();
+  
 
     public List<string> deck;
 
@@ -19,8 +41,8 @@ public class Solitaire : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-         PlayCards();
+        bottomRow = new List<string>[] {bottomCardRow1, bottomCardRow2, bottomCardRow3, bottomCardRow4, bottomCardRow5, bottomCardRow6, bottomCardRow7};
+        PlayCards();
         
     }
 
@@ -41,6 +63,8 @@ public class Solitaire : MonoBehaviour
             Debug.Log(card);
 
         }
+        SolitaireSort();
+        StartCoroutine(SolitaireDeal());
 
     }
 
@@ -76,6 +100,61 @@ public class Solitaire : MonoBehaviour
             T temp = list[randomNextCount];
             list[randomNextCount] = list[lsitCount];
             list[lsitCount] = temp;
+            
+        }
+
+    }
+
+    IEnumerator SolitaireDeal() {
+
+        for (int counter = 0; counter < 7; counter++) {
+
+            float yOffset = 0;
+            float zOffset = 0.03f;
+
+            foreach (string card in bottomRow[counter]) {
+
+                yield return new WaitForSeconds(0.03f);
+
+                GameObject newCard = Instantiate(cardPrefab, new Vector3 (bottomCardPossitions[counter].transform.position.x, 
+                    bottomCardPossitions[counter].transform.position.y - yOffset, bottomCardPossitions[counter].transform.position.z - zOffset), Quaternion.identity, 
+                    bottomCardPossitions[counter].transform);
+
+                newCard.name = card;
+
+                if ( card == bottomRow[counter][bottomRow[counter].Count - 1]) {
+
+                    newCard.GetComponent<Selectable>().faceUp = true;
+
+                }
+                
+
+                yOffset += 0.3f;
+                zOffset += 0.03f;
+ 
+            }
+            
+        }
+
+
+    }
+
+    public Sprite[] GetCardFaces() {
+
+        return this.cardfaces;
+
+    }
+
+    void SolitaireSort() {
+
+        for (int outerCounter = 0; outerCounter < 7; outerCounter++) {
+
+            for (int innerCounter = outerCounter; innerCounter < 7; innerCounter++) {
+
+                bottomRow[innerCounter].Add(deck.Last<string>());
+                deck.RemoveAt(deck.Count - 1);
+                
+            }
             
         }
 
